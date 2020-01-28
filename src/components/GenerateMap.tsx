34db185f9e0529653f44a5f7898d396949dffb47
Jen <jengkarlong@gmail.com>
2020-01-29 00:10:53 +0800

    added user point icon/label

--- src/components/GenerateMap.tsx
+++ src/components/GenerateMap.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
+import { usePosition } from 'use-position';
+
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import VectorLayer from 'ol/layer/Vector';
+import { Point, Polygon, Circle } from 'ol/geom';
 import { Vector } from 'ol/source';
 import { fromLonLat } from 'ol/proj';
-import { usePosition } from 'use-position';
-
+import {Icon, Style, Text, Fill, Stroke} from 'ol/style';
+import Feature from 'ol/Feature';
+import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
 import * as ol from 'ol';
 
 import '../css/ol.css';
@@ -27,17 +31,11 @@ interface State {
 };
 
 export const GenerateMap = (props) => {
-  let { latitude, longitude, error } = usePosition();
-  // if (props.longitude && props.latitude) {
-  //   latitude = props.latitude
-  //   longitude = props.longitude
-  // }
-  console.log(latitude, longitude, "ASD")
-  // React.useEffect(() => {
-  //   if (latitude && longitude && !error) {
-  //         TODO: Do something here?
-  //   }
-  // }, []);
+  let { latitude, longitude, error } = usePosition(); // This will get user's current location
+  if (props.longitude && props.latitude) {
+    latitude = props.latitude
+    longitude = props.longitude
+  }
 
   return <Locate latitude={latitude} longitude={longitude} />
 };
@@ -51,37 +49,53 @@ export const GenerateMap = (props) => {
   }
 
   state: State = {
-    center: [this.props.latitude, this.props.longitude],
+    center: this.getCenter(),
     zoom: this.props.zoom
   }
 
-  componentDidMount() {
-    this.initiateOpenLayers()
+  getCenter() {
+    return fromLonLat([this.props.longitude, this.props.latitude])
   }
 
-  componentDidUpdate() {
+  componentDidMount() {
+    // this.initiateOpenLayers()
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousProps !== this.props)
+        this.setState({center: this.getCenter()})
     this.initiateOpenLayers()
   }
 
   initiateOpenLayers() {
     this.resetMap();
-    console.log(this.props)
-    let coord = [ this.props.longitude, this.props.latitude ];
-    let zoom = this.props.zoom;
-    let center = fromLonLat(coord);
-    console.log(center)
+    console.log(this.state)
+    const { center, zoom } = this.state;
+
+    const circle = new Feature({
+      geometry: new Circle(center, 1000),
+    });
+
     let featuresLayer = new VectorLayer({
       source: new Vector({
-        features:[],
+        features:[
+          this.generateUserIcon(),
+          circle
+        ],
       })
     });
 
+    let streetLayer = new TileLayer({
+      source: new OSM()
+    })
+
     new Map({
       target: 'map',
+      interactions: defaultInteractions().extend([
+        new DragRotateAndZoom()
+      ]),
       layers: [
-        new TileLayer({
-          source: new OSM()
-        }),
+        streetLayer,
         featuresLayer
       ],
       view: new View({
@@ -90,6 +104,45 @@ export const GenerateMap = (props) => {
       })
     });
   }
+
+
+  generateUserIcon() {
+    const { center } = this.state
+
+    const userIcon = new Feature({
+      geometry: new Point(center),
+      name: 'Null Island',
+      population: 4000,
+      rainfall: 500
+    });
+
+    const userIconStyle = new Style({
+      image: new Icon({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: '/src/images/user-icon.png'
+      }),
+      text: new Text({
+        text: "This is you",
+        offsetY: -60,
+        font: 'bold 14px sans-serif',
+        padding: [5, 5, 5, 5],
+        backgroundStroke: new Stroke({
+          lineJoin: 'round',
+        }),
+        fill: new Fill({
+            color: '#000'
+        }),
+        backgroundFill: new Fill({
+          color: '#FA9B00'
+        })
+      })
+    });
+
+    userIcon.setStyle(userIconStyle);
+    return userIcon
+}
 
   resetMap() {
     document.getElementById('map').innerHTML = ""
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from "react-dom";
 import { GenerateMap } from "./components/GenerateMap";
 
 ReactDOM.render(
-    <GenerateMap  longitude={63.4281195} latitude={-20.2885533} />,
+    <GenerateMap />,
     document.getElementById("root")
 );
