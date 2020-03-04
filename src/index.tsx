79d1ea7c6b00b6b4f497e01cbf1ad447fec3a10f
Jen <jengkarlong@gmail.com>
2020-03-04 23:08:28 +0800

    add some todo comments

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import Development from "./components/Development";
+// Todo: transfer devel to another react /app folder
+// Todo: add import routes for all module component
 
 ReactDOM.render(
     <Development  />,
